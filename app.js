04c48505ba830538c3b7f95c3229a259581ebd9b
johnson0912 <johnsondeborde09@gmail.com>
2024-10-16 17:17:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Routes Here
 const authRoutes = require('./routes/authRoutes');
@@ -10,22 +11,27 @@ const courseRoutes = require('./routes/courseRoute');
 
 const app = express();
 app.use(bodyParser.json());
-const cors = require('cors');
 app.use(cors());
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.send('Johnson Deborde, MIT');
 });
 
 // Endpoint Here
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoute);
-app.use('/api/department',deptRoutes);
-app.use('/api/course',courseRoutes);
-app.use('/api/student',studentRoutes);
-const PORT = 3000;
+app.use('/api/department', deptRoutes);
+app.use('/api/course', courseRoutes);
+app.use('/api/student', studentRoutes);
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal Server Error' });
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-
 });
