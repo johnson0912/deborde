a7e1a2494493602a30a896fc6150775e0b998f27
Johnson Deborde <jdeborde@gbox.ncf.edu.ph>
2024-10-16 15:12:35 +0800

    modified app.js

--- app.js
+++ app.js
@@ -3,21 +3,29 @@ const bodyParser = require('body-parser');
 
 // Routes Here
 const authRoutes = require('./routes/authRoutes');
+const userRoute = require('./routes/userRoutes');
+const deptRoutes = require('./routes/deptRoutes');
+const studentRoutes = require('./routes/studentRoutes');
+const courseRoutes = require('./routes/courseRoute');
 
 const app = express();
 app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
-app.get('/', function(req, res){
-    res.send("Johnson Deborde, MIT");
+app.get('/', function(req, res) {
+    res.send('Johnson Deborde, MIT');
 });
 
 // Endpoint Here
-app.use('/api/user', authRoutes);
-
-const PORT = 5000;
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoute);
+app.use('/api/department',deptRoutes);
+app.use('/api/course',courseRoutes);
+app.use('/api/student',studentRoutes);
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+
 });
